78f41624c02cc3b4dedc91f8f1626bb0f965079f
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-08-02 16:38:40 -0400

    Fixing ip setup for nodes

--- data_node/ip_setup.sh
+++ data_node/ip_setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo ip addr add 192.168.58.102 dev ens4
-sudo ip addr add 192.168.57.102 dev ens5
-sudo ip addr add 192.168.56.102 dev ens6
+sudo ip addr add 192.168.58.102/24 dev ens4
+sudo ip addr add 192.168.57.102/24 dev ens5
+sudo ip addr add 192.168.56.102/24 dev ens6
--- od-controller/ip_setup.sh
+++ od-controller/ip_setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-sudo ip addr add 192.168.58.101 dev ens4
-sudo ip addr add 192.168.57.101 dev ens5
-sudo ip addr add 192.168.56.101 dev ens6
+sudo ip addr add 192.168.58.101/24 dev ens4
+#sudo ip addr add 192.168.57.101 dev ens5
+#sudo ip addr add 192.168.56.101 dev ens6
+
