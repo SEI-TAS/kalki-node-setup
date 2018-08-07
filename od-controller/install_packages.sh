31dee0a8c8362736d41c80c9eb3902dfe12eb70f
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-08-07 14:17:12 -0400

    Improving instructions to setup nodes

--- od-controller/install_packages.sh
+++ od-controller/install_packages.sh
@@ -13,7 +13,11 @@ install_python() {
     echo "Python Install Complete"
 }
 
-
+# Note that for the libvirt-bin daemon to actually listen to incoming connections, the following config changes are needed:
+# - /etc/libvirt/libvirtd.conf: uncomment the line #listen_tcp = 1
+# - /etc/libvirt/libvirtd.conf: uncomment the line #listen_tls = 0
+# - /etc/libvirt/libvirtd.conf: uncomment the line and change to #auth_tcp = "none
+# - /etc/default/libvirt-bin: add -l after libvirtd_opts, and uncomment.
 install_libvirt() {
     echo "Installing Libvirt..."
     sudo apt-get -yqq install libvirt-dev
@@ -35,3 +39,10 @@ install_python
 install_libvirt
 install_postgres
 echo "Finished setting up packages"
+
+# Also needed for network simple config:
+# 1 - Disable Network Manager:
+#  a- sudo systemctl stop NetworkManager.service
+#  b- sudo systemctl disable NetworkManager.service
+# 2 - Copy proper interfaces file to /etc/network/interfaces
+# 3 - Reboot node
