649e73a6c657b54e08af113ca0c0b592d17edace
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-02-13 13:41:38 -0500

    Test script to both start a VM and reditect ovs output to it

---
+++ control_node/start_umbox_and_redirect.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DATA_NODE_IP=192.168.58.102
+UMBOX_IMAGE_NAME=umbox-sniffer
+IOT_DEVICE_DB_ID=1
+IOT_DEVICE_IP=192.168.56.103
+
+UMBOX_OVS_PORT="$(python umbox.py -c start -s $DATA_NODE_IP -d $IOT_DEVICE_DB_ID -i $UMBOX_IMAGE_NAME)"
+python ovs.py -c add_rule -s $DATA_NODE_IP -d $IOT_DEVICE_IP -o $UMBOX_OVS_PORT
