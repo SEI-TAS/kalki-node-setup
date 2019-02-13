16b1ccbbfdd48da9952557d53881c3850dbab6d4
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-02-13 13:46:37 -0500

    Logging

--- control_node/start_umbox_and_redirect.sh
+++ control_node/start_umbox_and_redirect.sh
@@ -6,4 +6,5 @@ IOT_DEVICE_DB_ID=1
 IOT_DEVICE_IP=192.168.56.103
 
 UMBOX_OVS_PORT="$(python umbox.py -c start -s $DATA_NODE_IP -d $IOT_DEVICE_DB_ID -i $UMBOX_IMAGE_NAME)"
+echo "${UMBOX_OVS_PORT}"
 python ovs.py -c add_rule -s $DATA_NODE_IP -d $IOT_DEVICE_IP -o $UMBOX_OVS_PORT
