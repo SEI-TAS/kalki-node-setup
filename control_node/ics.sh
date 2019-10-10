b8520a5ce9e1a8821926726a575c710f60c34093
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-10 15:33:49 -0400

    Simple ICS config

---
+++ control_node/ics.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo iptables -A FORWARD -o enp0s31f6 -i enp2s0f0 -s 10.27.153.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -o enp0s31f6 -j MASQUERADE
