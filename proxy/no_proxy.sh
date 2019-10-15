483a62f02d6b18b254ad33390c644fd2f59fb2d3
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-15 13:26:31 -0400

    Adding gradle, apt, and integrated scripts

--- proxy/no_proxy.sh
+++ proxy/no_proxy.sh
@@ -1,6 +1,16 @@
+#!/usr/bin/env bash
 
 export http_proxy=''
 export https_proxy=''
 export HTTP_PROXY=''
 export HTTPS_PROXY=''
+
+sudo rm /etc/apt/apt.conf.d/01proxy
+
+cd docker
+bash docker_removal.sh
+
+cd ..
+cd gradle
+bash gradle_removal.sh
 
--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,11 +1,16 @@
-export GRADLE_OPTS="-Dhttp.proxyUrl=http://proxy.sei.cmu.edu -Dhttp.proxyPort=8080 -Dhttps.proxyUrl=http://proxy.sei.cmu.edu -Dhttps.proxyPort=8080"
-# NOTE: this is only needed if behind a proxy.
+#!/usr/bin/env bash
 PROXY="http://proxy.sei.cmu.edu:8080"
-#echo "Acquire::http::Proxy \"${PROXY}\";" | sudo tee /etc/apt/apt.conf
 
-# NOTE: this is only needed if behind a proxy.
 export http_proxy=${PROXY}
 export https_proxy=${PROXY}
 export HTTP_PROXY=${PROXY} 
 export HTTPS_PROXY=${PROXY}
 
+sudo cp 01proxy /etc/apt/apt.conf.d/
+
+cd docker
+bash docker_setup.sh
+
+cd ..
+cd gradle
+bash gradle_setup.sh
