dacdf6d6b44fb0d035f4ece0208dbcae147ee54e
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-30 10:22:02 -0400

    Improving bridge removal script

--- data_node/remove_bridge.sh
+++ data_node/remove_bridge.sh
@@ -3,14 +3,21 @@
 IOT_NIC=enp2s0f0
 EXT_NIC=enp2s0f1
 
+IOT_NIC_IP=10.27.151.127
+EXT_NIC_IP=10.27.152.6
+
+IOT_NIC_BROADCAST=10.27.151.255
+EXT_NIC_BROADCAST=10.27.152.255
+
 sudo ovs-vsctl del-br ovs-br
-sudo ovs-vsctl del-br ovs-iot
-sudo ovs-vsctl del-br ovs-ext
 
 sudo ifdown $EXT_NIC
 sudo ifup $EXT_NIC
 sudo ifdown $IOT_NIC
 sudo ifup $IOT_NIC
 
-cd hertz
-bash kalki_network.sh
+sudo ip addr add ${IOT_NIC_IP}/24 dev $IOT_NIC
+sudo ip addr add ${IOT_NIC_IP}/24 dev $EXT_NIC
+
+sudo ifconfig $IOT_NIC broadcast ${IOT_NIC_BROADCAST}
+sudo ifconfig $EXT_NIC broadcast ${EXT_NIC_BROADCAST}
