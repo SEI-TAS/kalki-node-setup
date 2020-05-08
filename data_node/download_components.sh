24b1de9d3a08963d45a0d2147a665295649f3d1d
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 15:27:28 -0400

    Avoiding retyping git user

--- data_node/download_components.sh
+++ data_node/download_components.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+GIT_USER=$1
+
 get_and_update_repo() {
   local repo_name="$1"
+  local git_user="$2"
 
   if [ ! -d "${repo_name}" ]; then
-    git clone https://github.com/SEI-TAS/${repo_name}.git
+    git clone https:/${git_user}@github.com/SEI-TAS/${repo_name}.git
   fi
 
   (cd ${repo_name} && git checkout dev && git pull)
@@ -12,5 +15,5 @@ get_and_update_repo() {
 
 (mkdir -p ../../kalki-repos/dn && \
  cd ../../kalki-repos/dn && \
- get_and_update_repo "kalki-iot-interface" && \
- get_and_update_repo "kalki-umbox-controller")
+ get_and_update_repo "kalki-iot-interface" ${GIT_USER} && \
+ get_and_update_repo "kalki-umbox-controller" ${GIT_USER} )
