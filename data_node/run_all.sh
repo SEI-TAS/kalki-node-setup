f32325c3e4c9e037793928b18c9e8177903d01b7
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 15:59:46 -0400

    Path fixing

--- data_node/run_all.sh
+++ data_node/run_all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-source ../../kalki-repos/kalki-umbox-controller/ovs-docker-server/prepare_env.sh
+BASE_PATH=../../kalki-repos/dn
+
+source ${BASE_PATH}/kalki-umbox-controller/ovs-docker-server/prepare_env.sh
 
 export HOST_TZ=$(cat /etc/timezone)
-docker-compose up -d -f ../../kalki-repos/kalki-iot-interface/docker-compose.yml \
-                     -f ../../kalki-repos/kalki-umbox-controller/ovs-docker-server/docker-compose.yml
+docker-compose up -d -f ${BASE_PATH}/kalki-iot-interface/docker-compose.yml \
+                     -f ${BASE_PATH}/kalki-umbox-controller/ovs-docker-server/docker-compose.yml
 bash compose_logs.sh
