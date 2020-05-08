c586f9a947d6068c329a717175062a431eceafa9
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 15:18:32 -0400

    Fixing download issues

--- data_node/download_components.sh
+++ data_node/download_components.sh
@@ -4,18 +4,14 @@ get_and_update_repo() {
   local repo_name="$1"
 
   if [ ! -d "${repo_name}" ]; then
-    git clone git@github.com:SEI-TAS/${repo_name}.git
+    git clone https://github.com/SEI-TAS/${repo_name}.git
     git checkout dev
   fi
 
   git pull
-
 }
 
-mkdir -p repos
-cd repos
-
-get_and_update_repo "kalki-iot-interface"
-get_and_update_repo "kalki-umbox-controller"
-
-cd ..
+(mkdir -p ../kalki-repos && \
+ cd ../kalki-repos && \
+ get_and_update_repo "kalki-iot-interface" && \
+ get_and_update_repo "kalki-umbox-controller")
