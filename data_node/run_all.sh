c4a35edebf7c22ee22cf74045f0254d93993a0f7
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 16:19:18 -0400

    Correct way to get back to folder

--- data_node/run_all.sh
+++ data_node/run_all.sh
@@ -4,7 +4,7 @@ BASE_PATH=../../kalki-repos/dn
 
 cd ${BASE_PATH}/kalki-umbox-controller/ovs-docker-server/
 source prepare_env.sh
-cd ..
+cd ../../../../kalki-node-setup/data_node
 
 export HOST_TZ=$(cat /etc/timezone)
 docker-compose up -d -f ${BASE_PATH}/kalki-iot-interface/docker-compose.yml \
