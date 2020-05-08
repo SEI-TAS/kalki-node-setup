b00eef21ed3b81d1768d218f7d55814fd183fda0
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 16:51:08 -0400

    Fixing env setup

--- data_node/run_all.sh
+++ data_node/run_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(cd dist/ovs-docker-server/ && source prepare_env.sh)
+cd dist/ovs-docker-server/ && source prepare_env.sh && cd ../..
 
 export HOST_TZ=$(cat /etc/timezone)
 docker-compose -f dist/kalki-iot-interface/docker-compose.yml \
