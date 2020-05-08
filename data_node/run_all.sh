538049ac02a07bc287e735ac3cf32696e7dbc169
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 16:55:36 -0400

    Preventing build of dist

--- data_node/run_all.sh
+++ data_node/run_all.sh
@@ -5,5 +5,5 @@ cd dist/ovs-docker-server/ && source prepare_env.sh && cd ../..
 export HOST_TZ=$(cat /etc/timezone)
 docker-compose -f dist/kalki-iot-interface/docker-compose.yml \
                -f dist/ovs-docker-server/docker-compose.yml \
-               up -d
+               up -d --no-build
 bash compose_logs.sh
