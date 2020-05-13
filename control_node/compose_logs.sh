ee3bccefc32e394211312ef2717a57ad9ddfaeed
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-12 23:27:57 -0400

    Fixed compose logs script

--- control_node/compose_logs.sh
+++ control_node/compose_logs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-BASE_PATH=../submodules
+source ../run_functions.sh
 MERGED_FILES=$(merge_docker_files "kalki-db" "kalki-umbox-controller" "kalki-main-controller" "kalki-device-controller")
 docker-compose ${MERGED_FILES} logs -f
--- data_node/compose_logs.sh
+++ data_node/compose_logs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-BASE_PATH=../submodules
+source ../run_functions.sh
 MERGED_FILES=$(merge_docker_files "kalki-iot-interface" "ovs-docker-server")
 docker-compose ${MERGED_FILES} logs -f
