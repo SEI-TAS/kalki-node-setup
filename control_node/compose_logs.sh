eff3b183cb0788b95edaad40d47ec0acb44977f1
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-19 11:37:08 -0400

    Attempting to use extends with healthcheck

--- control_node/compose_logs.sh
+++ control_node/compose_logs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source ../run_functions.sh
-MERGED_FILES=$(merge_docker_files "kalki-umbox-controller" "kalki-main-controller" "kalki-device-controller")
+MERGED_FILES=$(merge_docker_files "kalki-db" "kalki-umbox-controller" "kalki-main-controller" "kalki-device-controller")
 docker-compose ${MERGED_FILES} logs -f
--- control_node/run_all.sh
+++ control_node/run_all.sh
@@ -14,12 +14,12 @@ export HOST_TZ=$(cat /etc/timezone)
 
 # First start kalki-postgres container separately, and wait for it.
 # https://github.com/docker-library/postgres/issues/146
-(cd $DIST_PATH/kalki-db && bash run_postgres_container.sh)
-bash wait_for_postgres.sh kalki-postgres
+#(cd $DIST_PATH/kalki-db && bash run_postgres_container.sh)
+#bash wait_for_postgres.sh kalki-postgres
 
 # Now start the rest
-MERGED_FILES=$(merge_docker_files "kalki-umbox-controller" "kalki-main-controller" "kalki-device-controller")
-docker-compose ${MERGED_FILES} up -d --no-build
+MERGED_FILES=$(merge_docker_files "kalki-db" "kalki-umbox-controller" "kalki-main-controller" "kalki-device-controller")
+docker-compose ${MERGED_FILES} -f docker-compose.yml up -d --no-build
 
 # Show logs.
 bash compose_logs.sh
