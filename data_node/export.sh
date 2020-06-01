82cfa228b5d923e7e9f0b807482db511b91bd205
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-06-01 14:29:29 -0400

    Typo

--- data_node/export.sh
+++ data_node/export.sh
@@ -14,7 +14,7 @@ cp compose_logs.sh ${EXPORT_FOLDER}/
 cp install_packages.sh ${EXPORT_FOLDER}/
 cp README.md ${EXPORT_FOLDER}/
 
-cp -R ./dist cp ${EXPORT_FOLDER}/dist
+cp -R ./dist cp ${EXPORT_FOLDER}/
 
 export_image "kalki-iot-interface"
 export_image "ovs-docker-server"
