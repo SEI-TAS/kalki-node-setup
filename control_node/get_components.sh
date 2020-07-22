545dca3d8bf585e0607ac21dd670e6c0f4b6cfc3
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-07-22 14:38:50 -0400

    Correct way to get a tag

--- control_node/get_components.sh
+++ control_node/get_components.sh
@@ -7,7 +7,7 @@ source ../build_functions.sh
 init_submodules
 
 # Get updated repo info.
-BRANCH=1.6.0
+BRANCH=tags/1.6.0
 update_repo "kalki-db" $BRANCH
 update_repo "kalki-umbox-controller" $BRANCH
 update_repo "kalki-main-controller" $BRANCH
