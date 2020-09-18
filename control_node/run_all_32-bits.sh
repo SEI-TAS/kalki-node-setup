29774459de353e68e6eca69f090781e6565f768f
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-18 16:49:00 -0400

    Added specific run all to avoid deleting db

---
+++ control_node/run_all_32-bits.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -f run_functions.sh ]; then
+  cp ../run_functions.sh .
+fi
+
+# Include functions.
+source run_functions.sh
+
+# Prepare envs for each component.
+prepare "kalki-umbox-controller"
+prepare "kalki-main-controller"
+prepare "kalki-device-controller"
+
+# Start them all in compose.
+export HOST_TZ=$(cat /etc/timezone)
+export CMD_PARAMS="$@"
+docker-compose up -d --no-build
+
+# Show logs.
+bash compose_logs.sh
