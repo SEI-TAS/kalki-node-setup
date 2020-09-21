1d881501e7b712bbdfcafdc2466ae2d6b7c4836c
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-21 10:48:01 -0400

    Copy by file

--- control_node/nodb_run_all.sh
+++ control_node/nodb_run_all.sh
@@ -10,7 +10,7 @@ prepare "kalki-main-controller"
 prepare "kalki-device-controller"
 
 # Copy kalki-umboxcontroller tests in case they are needed.
-cp -r dist/kalki-umbox-controller/tests/ ./tests/
+cp dist/kalki-umbox-controller/tests/* ./tests/
 
 # Start them all in compose.
 export HOST_TZ=$(cat /etc/timezone)
